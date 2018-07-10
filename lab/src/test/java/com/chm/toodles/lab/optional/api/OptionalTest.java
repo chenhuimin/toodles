b1e8b5ad9a5f67f1c19c2baa3c33dcaa1b8409a2
chenhuimin <349196770@qq.com>
2018-07-10 08:35:06 +0800

    添加日志

--- lab/src/test/java/com/chm/toodles/lab/optional/api/OptionalTest.java
+++ lab/src/test/java/com/chm/toodles/lab/optional/api/OptionalTest.java
@@ -1,6 +1,8 @@
 package com.chm.toodles.lab.optional.api;
 
+import com.chm.toodles.lab.optional.pojo.Address;
 import com.chm.toodles.lab.optional.pojo.User;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 
 import java.util.Optional;
@@ -12,12 +14,25 @@ import java.util.Optional;
  * @date 2018-07-03 14:50
  */
 
+@Slf4j
 public class OptionalTest {
     @Test
-    public void orElseAndorElseGet() {
-        User user = null;
+    public void orElseAndorElseGet() throws Exception {
+        User user = createUser();
         user = Optional.ofNullable(user).orElse(createUser());
         user = Optional.ofNullable(user).orElseGet(() -> createUser());
+        user = Optional.ofNullable(user).orElseThrow(() -> new Exception("用户不存在"));
+
+    }
+
+    @Test
+    public void getCity() {
+        User user = createUser();
+        Address address = createAddress();
+        user.setAddress(address);
+        String city = Optional.ofNullable(user).map(u -> u.getAddress()).map(a -> a.getCity()).orElseGet(() -> null);
+        log.info("city={}", city);
+
 
     }
 
@@ -25,5 +40,11 @@ public class OptionalTest {
         User user = new User();
         user.setName("zhangsan");
         return user;
+    }
+
+    private Address createAddress() {
+        Address address = new Address();
+        address.setCity("Shanghai");
+        return address;
     }
 }
