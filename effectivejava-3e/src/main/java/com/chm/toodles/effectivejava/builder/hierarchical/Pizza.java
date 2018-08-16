068135b2cfdafce54f5f8db8ca5ccc1655d9d02a
chenhuimin <349196770@qq.com>
2018-08-16 08:27:57 +0800

    builder模式创建对象

---
+++ effectivejava-3e/src/main/java/com/chm/toodles/effectivejava/builder/hierarchical/Pizza.java
@@ -0,0 +1,19 @@
+package com.chm.toodles.effectivejava.builder.hierarchical;
+
+import java.util.Set;
+
+public abstract class Pizza {
+
+  public enum Topping {
+    HAM, MUSHROOM, ONION, PEPPER, SAUSAGE
+  }
+
+  protected final Set<Topping> toppings;
+
+  public abstract static Builder
+
+  {
+
+  }
+
+}
